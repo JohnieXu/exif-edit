f9c6acaccbe55313bb8d129aeed404f4c1e0233a
JohnieXu <281910378@qq.com>
2026-04-02 08:24:18 +0800

    feat: enhance Vite configuration and update dependencies for Vue 3

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,30 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          sentry: ['@sentry/vue'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', '@sentry/vue'],
+    exclude: [],
+    force: true,
+  },
+  ssr: {
+    noExternal: ['@sentry/vue'],
+  },
+  define: {
+    __VUE_OPTIONS_API__: false,
+    __VUE_PROD_DEVTOOLS__: false,
+  },
 })
